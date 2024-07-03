d5f13ede42f718c18e8ddc6cde4089e6ba036f1c
Ralf Podchull <app@projectdelight.de>
2024-07-03 11:34:14 +0200

    4.5.3.IV.A2 added

---
+++ aufgaben_4_4_3/lib/4_5_3_IV_A2.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('AlertDialog Beispiel'),
+        ),
+        body: Center(
+          child: MyHomePage(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  void _showAlertDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('AlertDialog Titel'),
+          content: Text('Dies ist eine Nachricht im AlertDialog.'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Abbrechen'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () => _showAlertDialog(context),
+      child: Text('Alarmaaaaa!'),
+    );
+  }
+}
